a7bf27c517ec3b79b541d741ebac861c97bc7096
NicoF46 <49656253+NicoF46@users.noreply.github.com>
2021-10-16 22:26:06 -0300

    Upload to do list code (#1)
    * upload gitignore file
    
    * upload code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1> My Todos</h1>
+      <Todo text="TAREA 1" />
+      <Todo text="TAREA 2" />
+      <Todo text="TAREA 3" />
+    </div>
+  );
+}
+
+export default App;
